66698cc528867817d1891490438446b0c58f8bdb
moonjs1578 <moonjs1578@gmail.com>
2024-11-25 14:52:12 +0900

    Add files via upload

--- webcrawling/ch03_daum_news_list.py
+++ webcrawling/ch03_daum_news_list.py
@@ -2,17 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 from fnc_news import get_news_info
 
-url='https://news.daum.net/breakingnews/digital'
+url="https://news.daum.net/breakingnews/digital"
 result=requests.get(url)
-doc=BeautifulSoup(result.text,'html.parser')
+doc=BeautifulSoup(result.text, "html.parser")
 
-# HTML->하이퍼링크(a 태그 href 속성)
+# HMTL → 하이퍼링크(a 태그 href 속성)
 
-link_list=doc.select('ul.list_news2 a.link_txt')   #꺽쇠를 쓰거나 이거는 안됨/ 띄어쓰기인데 자손으로
+link_list=doc.select("ul.list_news2 a.link_txt")
 
 print(len(link_list))
-for i,link in enumerate(link_list):
-    print(f'{i+1}==============================================')      
-    
-    get_news_info(link['href'])
-    
+for i, link in enumerate(link_list):
+    print(f"{i+1} ========================================")
+    get_news_info(link["href"])
--- webcrawling/fnc_news.py
+++ webcrawling/fnc_news.py
@@ -1,34 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_news_info(url:str):
 
+def get_news_info(url : str):
     result=requests.get(url)
-    doc=BeautifulSoup(result.text,'html.parser')
-
-    title= doc.select("h3.tit_view")[0].get_text() #문자섞여있는것중에 get text 를써서 텍스트만 가져감
-    
-    contents=doc.select('section > p')
-    content=''
+    doc=BeautifulSoup(result.text, "html.parser")
+    title=doc.select("h3.tit_view")[0].get_text()
+    contents=doc.select("section > p")
+    content=""
     for text in contents:
-        content += text.get_text()
-
-    #기자:
-    writer_list=doc.select('span.txt_info')
-    if len(writer_list)<2:
-        writer=''
-    else:
-        writer=writer_list[0].get_text()
+        content += (text.get_text())
         
- 
+    # 기자, 날짜 → span.txt_info → [0]기자, [1]날짜
+    # 날짜       → span.txt_info → [0]날짜 → ""
+    writer_list = doc.select("span.txt_info")
+    if len(writer_list) < 2 :
+        writer=""
+    else :
+        writer=writer_list[0].get_text()
+    writer=doc.select("span.txt_info")[0].get_text()
+    reg_date=doc.select("span.num_date")[0].get_text()
+    print(reg_date) #20241028
+    split_list=reg_date.split(".") 
+    split_date=list(map(lambda x : x.strip(), split_list))
+    reg_date=split_date[0]+split_date[1]+split_date[2]
+    print(f"뉴스 제목 : {title}")
+    print(f"뉴스 기자 : {writer}")
+    print(f"뉴스 날짜 : {reg_date}")
+    print(f"뉴스 본문 : {content}")
 
-    reg_date=doc.select('span.num_date')[0].get_text()
-    split_list=reg_date.split('. ') #결과['2024', ' 10', ' 28', ' 14:42'] #구분자를 '.'으로 두면 빈카이 생기니까 자르는 거를'.공백'으로 하면된다
-    reg_date=split_list[0]+split_list[1]+split_list[2]
 
-
-
-    print(f'뉴스제목:{title}')
-    print(f'뉴스기자:{writer}')
-    print(f'뉴스날짜:{reg_date}')
-    print(f'뉴스본문:{content}')
